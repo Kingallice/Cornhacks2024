4de9e21d3b63e6cb565b2c817d3cfe0049ba4be1
red2922 <inthavongsaj@lopers.unk.edu>
2024-02-17 15:29:12 -0600

    Letting Carissa work on GUI alone

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,32 +1,24 @@
-import os
+
 import azure.cognitiveservices.speech as speechsdk
 import Services.GetKeys as key
 import Services.GetConfiguration as config
+from JakeTestDirectory import jakeTkinter as window
 
 def recognize_from_microphone():
     running = True
     # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
     speech_config = speechsdk.SpeechConfig(subscription=key.GetAzureKey(), region=config.GetConfig("region"))
     speech_config.speech_recognition_language="en-US"
-
     audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    print("Please speak into your microphone.")
-
     while running:
+        result = speech_recognizer.recognize_once_async().get()
 
-        speech_recognition_result = speech_recognizer.recognize_once_async().get()
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            return str(result.text)
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            return("No speech could be recognized: {}".format(result.no_match_details))
 
-        if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-            print("Recognized: {}".format(speech_recognition_result.text))
-        elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-            print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-        elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-            cancellation_details = speech_recognition_result.cancellation_details
-            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-            if cancellation_details.reason == speechsdk.CancellationReason.Error:
-                print("Error details: {}".format(cancellation_details.error_details))
-                print("Did you set the speech resource key and region values?")
-
-recognize_from_microphone()
+#Everything just returns right now. 
+window.build_window(recognize_from_microphone())
