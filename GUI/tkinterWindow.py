37062b1a870ab2841213cf6de2029e92414fb3dc
carissalk <81452708+carissalk@users.noreply.github.com>
2024-02-17 15:03:43 -0600

    Update tkinterWindow.py

--- GUI/tkinterWindow.py
+++ GUI/tkinterWindow.py
@@ -5,16 +5,16 @@ def close():
 
 window = tkinter.Tk()
 
-label = tkinter.Label(window, text='Text on the screen', font=('Times New Roman','40'), fg='black', bg='white')
-close_button = tkinter.Button(window, text="STOP", command=close)
+label = tkinter.Label(window, text='Text on the screen', font=('ARIAL','20'), fg='black', bg='white')
+close_button = tkinter.Button(window, text="STOP", command=close, relief='groove', bg='grey', fg='white')
 window.overrideredirect(True)
 # window.geometry()
 # window.lift()
 window.wm_attributes("-topmost", True)
 
 
-label.pack()
-close_button.pack()
+label.pack(ipady=10, ipadx=10)
+close_button.pack(pady=5)
 window.mainloop()
 
 #window = Tk()
