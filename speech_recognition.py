3f5580d3d0422081361e8fffb47d2be6000f44e4
red2922 <inthavongsaj@lopers.unk.edu>
2024-02-17 23:13:32 -0600

    Updated ConfigurationService

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,13 +1,15 @@
 import azure.cognitiveservices.speech as speechsdk
 from recordaudio import record_audio
 
-
 import Services.KeyService as key
 from Services.ConfigurationService import Config
 from JakeTestDirectory import jakeTkinter as window
 
+configure = Config()
+
+
 def recognize_from_microphone():
-    speech_config = speechsdk.SpeechConfig(subscription=key.GetAzureKey(), region=Config().GetConfig("region"))
+    speech_config = speechsdk.SpeechConfig(subscription=key.GetAzureKey(), region=configure.GetConfig("region"))
     speech_config.speech_recognition_language = "en-US"
     audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
@@ -21,7 +23,7 @@ def recognize_from_microphone():
 # Everything just returns right now.
 
 def recognize_from_computer():
-    speech_config = speechsdk.SpeechConfig(subscription=key.GetAzureKey(), region=config.GetConfig("region"))
+    speech_config = speechsdk.SpeechConfig(subscription=key.GetAzureKey(), region=configure.GetConfig("region"))
     speech_config.speech_recognition_language = "en-US"
     audio_config = speechsdk.audio.AudioConfig(filename='output.wav')
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
