f175aa5db71bf3669bdea1a67501dde2b12165f5
carissalk <81452708+carissalk@users.noreply.github.com>
2024-02-17 21:30:32 -0600

    Update tkinterWindow.py
    Captioning display with tkinter Text.

--- GUI/tkinterWindow.py
+++ GUI/tkinterWindow.py
@@ -1,22 +1,43 @@
 import tkinter
+from tkinter import *
 
-def close():
-    window.destroy()
+def updateLines(targetText: Text, newLine):
+    targetText.tag_configure("center", justify='center')
+    lines = targetText.get('0.0',END).splitlines()
+    targetText.config(state='normal')
+    targetText.delete('0.0', END)
 
-window = tkinter.Tk()
-
-label = tkinter.Label(window, text='Text on the screen', font=('ARIAL','20'), fg='black', bg='white')
-close_button = tkinter.Button(window, text="STOP", command=close, relief='groove', bg='grey', fg='white')
-window.overrideredirect(True)
-# window.geometry()
-# window.lift()
-window.wm_attributes("-topmost", True)
+    idx = 0
+    if(len(lines) > 1):
+        idx = 1
+    targetText.insert('0.0', lines[idx] + "\n" + newLine, 'center')
+    targetText.config(state='disabled')
 
 
-label.pack(ipady=10, ipadx=10)
-close_button.pack(pady=5)
+def buildOverlay():
+
+    def close():
+        window.destroy()
+
+    window = tkinter.Tk()
+
+    #label = tkinter.Label(window, name='main', text='Text on the screen', font=('ARIAL','20'), fg='black', bg='white')
+    lines = Text(window, name= 'main', height=2, width=50, font= ('Arial', '20'))
+    lines.config(state='disabled')
+    updateLines(lines, "[begin BabbleBuddy live captioning]\n")
+    close_button = tkinter.Button(window, text="STOP", command=close, relief='groove', bg='grey', fg='white')
+    window.overrideredirect(True)
+    window.wm_attributes("-topmost", True)
+
+    lines.pack(ipadx=10)
+    close_button.pack(pady=5)
+
+    return window
+
+
+window = buildOverlay()
+
+#testing
+while True:
+   updateLines(window.children["main"], input())
 window.mainloop()
-
-#window = Tk()
-#window.wm_attributes('-transparentcolor','white')
-#window.config(bg="white")
